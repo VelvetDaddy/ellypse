c289d7b6c2c8bdc23024392e12cacab02f89a0a0
VelvetDaddy <150512459+VelvetDaddy@users.noreply.github.com>
2023-11-19 02:36:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 200,
+            "density": {
+                "enable": true,
+                "value_area": 1000
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#ffffff"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": false
+        },
+        "size": {
+            "value": 2,
+            "random": true,
+            "anim": {
+                "enable": false
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "enable": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false
+            },
+            "onclick": {
+                "enable": false
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
